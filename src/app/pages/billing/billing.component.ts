50589ff82dbf2e0fefc535539eda8f8cbee1e83d
mimi428 <mihika.ranjit1@gmail.com>
2025-03-21 11:55:24 +0545

    format table

--- src/app/pages/billing/billing.component.ts
+++ src/app/pages/billing/billing.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-billing',
+  imports:[CommonModule],
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.css']
 })
 export class BillingComponent {
+  rows: any[] = [];
   
   selectInput(event: Event): void {
     (event.target as HTMLInputElement).select();
@@ -13,5 +16,11 @@ export class BillingComponent {
 
   closePopup(): void {
     console.log("Popup closed");
+  }
+  addRow() {
+    this.rows.push({});
+  }
+  deleteRow(){
+    this.rows.pop();
   }
 }
