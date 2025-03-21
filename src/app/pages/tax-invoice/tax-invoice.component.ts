f3308241dea6ec77175729cb2367758a99a13f90
mimi428 <mihika.ranjit1@gmail.com>
2025-03-21 06:42:53 +0545

    invoicepage

--- src/app/pages/tax-invoice/tax-invoice.component.ts
+++ src/app/pages/tax-invoice/tax-invoice.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tax-invoice',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tax-invoice.component.html',
   styleUrl: './tax-invoice.component.css'
 })
 export class TaxInvoiceComponent {
+  invoiceForm: FormGroup;
+  costCenters = [
+    { id: 1, name: 'Finance' },
+    { id: 2, name: 'Operations' },
+    { id: 3, name: 'IT' },
+  ];
 
+  constructor(private fb: FormBuilder) {
+    this.invoiceForm = this.fb.group({
+      invDate: [{ value: '', disabled: true }],
+      entryDate: [{ value: '', disabled: true }],
+      soNo: [{ value: '', disabled: true }],
+      loadFullSODetail: [false],
+      warehouse: [{ value: '', disabled: true }],
+      salesman: [{ value: '', disabled: true }],
+      costCenter: [''],
+      customer: [{ value: '', disabled: true }],
+      address: [{ value: '', disabled: true }],
+      vatNo: [{ value: '', disabled: true }],
+      remarks: [''],
+      flatDisPercent: [{ value: '', disabled: true }],
+      flatDisRs: [{ value: '', disabled: true }],
+    });
+  }
 }
