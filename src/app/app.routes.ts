9610055cd1b95bd36bfb991eb72fae019e7cc127
mimi428 <mihika.ranjit1@gmail.com>
2025-03-23 10:28:46 +0545

    tabs in add-product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,6 @@ import { TopBarComponent } from './navbar/components/top-bar/top-bar.component';
 import { TransactionComponent } from './navbar/components/transaction/transaction.component';
 import { Component } from '@angular/core';
 import { AddProductComponent } from './pages/add-product/add-product.component';
-import { DetailedInfoComponent } from './pages/detailed-info/detailed-info.component';
-import { ImageUploadComponent } from './pages/image-upload/image-upload.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'master', pathMatch: 'full' },
@@ -19,8 +17,6 @@ export const routes: Routes = [
       { path: 'product-master', component: ProductMasterComponent},
       { path: 'tax-invoice', component: TaxInvoiceComponent },
       {path: 'add-product', component: AddProductComponent},
-        {path:'detailed-info', component:DetailedInfoComponent},
-        {path:'image-upload', component:ImageUploadComponent},
       ]
     }
   ]
--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -1,24 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { DetailedInfoComponent } from "../detailed-info/detailed-info.component";
-import { AlternateUnitComponent } from "../alternate-unit/alternate-unit.component";
-import { InventoryControlComponent } from "../inventory-control/inventory-control.component";
-import { ImageUploadComponent } from "../image-upload/image-upload.component";
-import { YieldComponent } from "../yield/yield.component";
-import { BatchwisePriceComponent } from "../batchwise-price/batchwise-price.component";
 
 @Component({
   selector: 'app-add-product',
-  imports: [CommonModule, DetailedInfoComponent, AlternateUnitComponent, InventoryControlComponent, ImageUploadComponent, YieldComponent, BatchwisePriceComponent],
+  imports: [CommonModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent { 
-  activeTab: string = 'Detailedinfo';
-  setActiveTab(tabName: string): void {
-    console.log('Setting active tab to:', tabName); 
-    this.activeTab = tabName;
+  selectedTab: string = 'detail'; // Default selected tab
+
+  // Function to switch tabs
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
   }
 
 }
