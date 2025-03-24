8dfe348b862361e185b941d9e0a55644e6cfd01d
mimi428 <mihika.ranjit1@gmail.com>
2025-03-24 11:01:31 +0545

    img upload , popup

--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -9,13 +9,39 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent { 
-  selectedTab: string = 'detail'; // Default 
-
+  selectedTab: string = 'detail'; // Default tab open 
   // Function to switch tabs
   selectTab(tab: string): void {
     this.selectedTab = tab;
   }
+  //for img displaying and popupss
+  imageSrc: string | ArrayBuffer | null = null; // image data URL hold garera rakhxa
+  showPopup: boolean = false; // popup visibile xa ki nai
+  isUploaded: boolean = false; // upload btn click vayo ki nai
+  selectedFile: File | null = null; // selected file k ho 
 
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imageSrc = reader.result; // Set the image data URL
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  uploadImage(): void {
+    if (this.selectedFile) {
+      console.log('Uploading file:', this.selectedFile.name);
+      this.isUploaded = true;
+      this.showPopup = true;
+    }
+  }
+  closePopup(): void {
+    this.showPopup = false;
+  }
   //for yield tab functionalityyyyyyyyyyyyy
   entries: { subItem: string, yield: string }[] = [];
   subItemControl = new FormControl('');
@@ -26,12 +52,11 @@ export class AddProductComponent {
     const yieldValue = this.yieldControl.value;
 
     if (subItemValue && yieldValue) {
-      // Add the new entry to the entries array
+      // Add the new entry push
       this.entries.push({
         subItem: subItemValue,
         yield: yieldValue
       });
-
       this.subItemControl.setValue('');
       this.yieldControl.setValue('');
     }
@@ -44,8 +69,8 @@ export class AddProductComponent {
   }
 
   removeEntry(index: number) {
-    // Remove the entry at the specified index
-    this.entries.splice(index, 1);
+    // Remove the entry
+    this.entries.splice(index, 1);//splice means 
   }
 
 
